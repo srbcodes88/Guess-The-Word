7254ef649e80fe09ad6d9651674f1ac6b5305b1e
srbcodes88 <psbbsrb170188@gmail.com>
2025-09-30 22:52:26 +0530

    login.py added

---
+++ login.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QLabel, QFormLayout, QMessageBox
+from PyQt5.QtCore import Qt
+from database import login_user
+from gui_register import RegisterWindow
+from gui_player import PlayerWindow
+from gui_admin import AdminWindow
+
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Login")
+        self.layout = QFormLayout()
+        self.username = QLineEdit()
+        self.password = QLineEdit()
+        self.password.setEchoMode(QLineEdit.Password)
+        self.btn_login = QPushButton("Login")
+        self.btn_register = QPushButton("Register")
+        self.layout.addRow("Username:", self.username)
+        self.layout.addRow("Password:", self.password)
+        self.layout.addRow(self.btn_login)
+        self.layout.addRow(self.btn_register)
+        self.setLayout(self.layout)
+        self.btn_login.clicked.connect(self.login)
+        self.btn_register.clicked.connect(self.open_register)
+
+    def login(self):
+        uname = self.username.text()
+        pwd = self.password.text()
+        role = login_user(uname, pwd)
+        if role:
+            self.close()
+            if role == "admin":
+                AdminWindow(uname).show()
+            else:
+                PlayerWindow(uname).show()
+        else:
+            QMessageBox.warning(self, "Login failed", "Wrong username or password.")
+
+    def open_register(self):
+        self.reg = RegisterWindow()
+        self.reg.show()
